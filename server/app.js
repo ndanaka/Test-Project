fe97bff80cbb5e7f5a129156d114e59016a25232
ndanaka <tanakanaoto632@gmail.com>
2025-01-22 09:36:53 +0800

    update app.js

--- server/app.js
+++ server/app.js
@@ -20,11 +20,13 @@ const user = require("./routes/userRoute");
 const product = require("./routes/productRoute");
 const order = require("./routes/orderRoute");
 const payment = require("./routes/paymentRoute");
+const dex = require("./routes/dexRoute");
 
 app.use("/api/user", user);
 app.use("/api/product", product);
 app.use("/api/order", order);
 app.use("/api/payment", payment);
+app.use("/api/dex", dex);
 
 // deployment
 __dirname = path.resolve();
